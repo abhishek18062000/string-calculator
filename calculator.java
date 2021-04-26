562a656717ffe9b424ef869b17dac53cac186906
abhis <abhishekmaurya18062000@gmail.com>
2021-04-26 16:09:50 +0530

    4.Suport different delimiters eg //;\n1;2

--- calculator.java
+++ calculator.java
@@ -8,19 +8,20 @@ class calculator
 		Scanner scn= new Scanner(System.in);
 		String str = new String(scn.next());
 		String s=handelN(str);
-		System.out.println(s);
+		//System.out.println(s);
 		int sum=Add(s);
 		System.out.println(sum);
 	}
 	
 	public static String handelN(String strWithN){
-		String r=strWithN.replace("\\", ",");
-		String s=r.replace('n',',');
-		return s;
+		String r=strWithN.replace("\\", ";");
+		String s=r.replace('n',';');
+		String t=s.replace('/',';');
+		return t;
 	}
 
 	public static int Add(String numbers){
-		String[] res = numbers.split("[,]", 0);
+		String[] res = numbers.split("[;]", 0);
 		int sum=0;
 		for(String num: res) {
 			if(num.equals("")){
