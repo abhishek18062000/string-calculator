452cd5b0ed5794647f33a9d079392e116002ea29
abhis <abhishekmaurya18062000@gmail.com>
2021-04-26 13:41:42 +0530

    String calculator to calculate SUM of two number seperated using comma for example '' or 1 or '1,2' as input

--- calculator.java
+++ calculator.java
@@ -1,9 +1,25 @@
+import java.util.*;
+import java.io.*;
+
 class calculator
 {
-	public static void main(sys argv[]){
-		
+	public static void main(String[] args){
+		Scanner scn= new Scanner(System.in);
+		String str=scn.next();
+		int sum=Add(str);
+		System.out.println(sum);
 	}
-	public static add(String numbers){
-		String a=numbers;
+	public static int Add(String numbers){
+		String[] res = numbers.split("[,]", 0);
+		int sum=0;
+		for(String num: res) {
+			if(num.equals("")){
+				sum=0;
+			}
+			else{
+				sum+=Integer.parseInt(num);
+			}
+       }
+	   return sum;
 	}
 }
